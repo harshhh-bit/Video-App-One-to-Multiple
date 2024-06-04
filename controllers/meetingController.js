732eec3c82a5dbe59746488647f728d8daff898c
Harshit Goel <harshitgoel003@gmail.com>
2024-06-04 10:40:22 +0530

    Save Everything

--- controllers/meetingController.js
+++ controllers/meetingController.js
@@ -3,23 +3,14 @@ const loadIndex = async(req,res) => {
 }
 
 const loadMeeting = async (req, res) => {
-    if (req.query.meetingID && req.query.meetingID.length === 8 && req.query.isHost) {
-        res.render('stream', { meeting_id: req.query.meetingID, username: "Host", is_host: req.query.isHost });
-    } else if (req.query.meetingID && req.query.meetingID.length === 8 && !req.query.isHost) {
-        res.render('meeting', { meeting_id: req.query.meetingID, is_host: req.query.isHost });
-    } else {
-        res.redirect('/');
-    }
-};
-
+    res.render('meeting', { meeting_id: req.query.meeting_id, is_host: req.query.is_host });
+}
 
 const loadStream = async(req,res) => {
-    if(req.query.meeting_id && req.query.meeting_id.length == 8 && !req.query.username)
-        res.redirect('/meeting?meetingID=' + req.query.meeting_id);
-    else if(req.query.meeting_id && req.query.meeting_id.length == 8 && req.query.username)
-        res.render('stream', { meeting_id: req.query.meeting_id, username: req.query.username, is_host: req.query.is_host });
+    if(!req.query.username)
+        res.redirect('/meeting'+'?'+'meeting_id='+req.query.meeting_id+'&'+'is_host='+req.query.is_host);
     else
-        res.redirect('/');
+        res.render('stream', { meeting_id: req.query.meeting_id, username: req.query.username, is_host: req.query.is_host });
 }
 
 module.exports = {
